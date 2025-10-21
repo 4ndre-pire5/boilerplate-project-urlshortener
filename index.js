65e1c999c6e2eaccba1f40dab8a8ede56ce6c99b
Andre Pires <erdnapfaserip@gmail.com>
2025-10-21 12:53:33 -0300

    Teste 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+
+const urlDatabase = {};
+let nextShortId = 1; // Contador para gerar o shortcode numérico sequencial
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -21,4 +26,68 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+// POST with body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+//app.use("/api/shorturl", bodyParser.json());
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+
+  if (!originalUrl) {
+    return res.json({ error: 'URL missing'});
+  }
+
+  //URL validation
+  const urlRegex = /^(http|https):\/\/([^ "]+)$/;
+  if (!urlRegex.test(originalUrl)) {
+    return res.json({ error: 'Invalid URL' });
+  }
+
+  //Extract URL
+  let hostname;
+  try {
+    hostname = new URL(originalUrl).hostname;
+  } catch (e) {
+    return res.json({ error: 'Invalid URL' });
+  }
+
+  //Verify hostname using dns
+  dns.lookup(hostname, (err, address) => {
+    if (err) {
+      console.error(`DNS lookup failed for ${hostname}:`, err.message);
+      return res.json({ error: "Invalid URL" });
+    }
+  });
+
+  // Save URL to database
+  const numericShortUrl = nextShortId;
+
+  // Save URL using short url as key
+  urlDatabase[numericShortUrl] = originalUrl;
+  nextShortId++;
+  
+  res.json({ 
+    original_url: originalUrl,
+    short_url: numericShortUrl 
+  });
+
+});
+
+// --- ROTA 2: GET /api/shorturl/:shortcode (REDIRECIONAMENTO) ---
+app.get("/api/shorturl/:shortcode", (req, res) => {
+  // 1. Obtém o shortcode da URL
+  const shortcode = req.params.shortcode;
+  const shortcodeNumber = parseInt(shortcode);
+
+  // 2. Verifica se o shortcode é um número válido e existe no banco de dados
+  if (isNaN(shortcodeNumber) || !urlDatabase[shortcodeNumber]) {
+      return res.status(404).json({ error: 'No short URL found for this code' });
+  }
+
+  // 3. Recupera a URL original
+  const originalUrl = urlDatabase[shortcodeNumber];
+
+  // 4. Redireciona o usuário para a URL original
+  res.redirect(originalUrl);
 });
